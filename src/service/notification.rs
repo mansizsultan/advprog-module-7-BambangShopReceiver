a23482924a5e3c2ea17d8065a840332d613e0703
mansizsultan <sultanibnumansiz@gmail.com>
2025-03-28 11:20:54 +0700

    Implement receive_notification function in Notification service

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -90,4 +90,10 @@ impl NotificationService {
                 e.to_string()))
         }
     }
+
+    pub fn receive_notification(payload: Notification) -> Result<Notification> {
+        let subscriber_result: Notification = NotificationRepository::add(payload);
+        return Ok(subscriber_result);
+    }
+
 }
